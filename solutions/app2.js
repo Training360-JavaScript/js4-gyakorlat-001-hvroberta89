bb7750767a867b8a41a45e04573f87c88d6dabd1
hvroberta89 <veber.roberta@gmail.com>
2021-11-17 17:49:52 +0100

    completed tasks

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,15 @@
-
+const sorterPromise = (array=[]) => 
+    new Promise((resolve, reject) => {
+    if (array.every((item) => typeof item === 'string')) {
+        resolve(array.sort())
+    } else {
+        reject('Error: Not all elements are string type!')
+    }
+});
 
 
 
 
 
 // export default sorterPromise;
+export default sorterPromise;
